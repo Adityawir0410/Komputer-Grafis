82f91c68a8d5cd799c73beb672b676bd3f549f43
Melon <alvianprasetio24@gmail.com>
2025-12-02 00:04:35 +0700

    Puja Vibe Coder

--- app/tour/layout.jsx
+++ app/tour/layout.jsx
@@ -1,6 +1,7 @@
 "use client";
 import { TourProvider } from './_context/TourContext';
 import HUD from './_components/HUD';
+import MiniMap from './_components/MiniMap';
 import ProtectedRoute from '../_components/ProtectedRoute';
 import { useEffect, useState } from 'react';
 import { usePathname } from 'next/navigation';
@@ -25,6 +26,9 @@ export default function TourLayout({ children }) {
         <div className="relative min-h-screen bg-gray-100">
           {/* Global HUD for VR pages */}
           {isVRPage && <HUD />}
+          
+          {/* Mini Map for VR pages */}
+          {isVRPage && <MiniMap />}
           
           {children}
         </div>
